d8842b556e360675b25c70164d79254416916f61
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2022-02-03 15:56:05 +0100

    Upload content
    Added openapi.yaml, api_example.R, index.html, README.md & CNAME

---
+++ README.md
@@ -0,0 +1,153 @@
+﻿# Arbeitsamt Weiterbildungssuche API 
+Die Bundesagentur für Arbeit verfügt über eine der größten Datenbanken für Weiterbildungsangebote in Deutschland. Obwohl sie vollständig staatlich ist und es sich dabei um einen sehr spannenden Basisdatensatz handelt, mit dem viele Analysen möglich wären, bietet die Bundesagentur für Arbeit dafür bis heute keine offizielle API an.
+
+
+## Authentifizierung
+Die Authentifizierung funktioniert per OAuth 2 Client Credentials mit JWTs.
+Die Client Credentials sind z.B. in der App hinterlegt:
+
+**ClientID:** 38053956-6618-4953-b670-b4ae7a2360b1
+
+**ClientSecret:** c385073c-3b97-42a9-b916-08fd8a5d1795
+
+```bash
+curl \
+-H 'Host: rest.arbeitsagentur.de' \
+-H 'Accept: */*' \
+-H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
+-H 'Accept-Language: de,en-US;q=0.7,en;q=0.3' \
+-H 'User-Agent:  Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' \
+--data-binary "client_id=38053956-6618-4953-b670-b4ae7a2360b1&client_secret=c385073c-3b97-42a9-b916-08fd8a5d1795&grant_type=client_credentials" \
+--compressed 'https://rest.arbeitsagentur.de/oauth/gettoken_cc'
+```
+
+## Jobbörse
+
+**URL:** https://web.arbeitsagentur.de/weiterbildungssuche/suche
+	
+
+Die Weiterbildungssuche ermöglicht verfügbare Weiterbildungsangebote mit verschiedenen get Parametern zu filtern:
+
+
+**Parameter:** *sw*  (Optional)
+Suchwort (z.B. IT-Security%2520-%2520allgemein)
+
+
+**Parameter:** *ort*  (Optional)
+Ortsangabe nebst Postleitzahl und Koordinaten (z.B. Feucht_90537_11.224918_49.376701)
+
+
+**Parameter:** *page* (Optional)
+Seite…
+
+
+**Parameter:** *size* (Optional)
+Anzahl der Ergebnisse
+
+
+### Filter
+
+
+**Parameter:** *re*  (Optional)
+- BW
+- BY
+- BE
+- BB
+- HB
+- HH
+- HE
+- MV
+- NI
+- NW
+- RP
+- SL
+- SN
+- ST
+- SH
+- TH
+
+Region/Bundesland: BW=Bade-Württemberg, BY=Bayern, BE=Berlin, BB=Brandenburg, HB=Bremen, HH=Hamburg, HE=Hessen, MV=Mecklenburg-Vorpommern, NI=Niedersachsen, NW=Nordrhei-Westfalen, RP=Rheinland-Pfalz, SL=Saarland, SN=Sachsen, ST=Sachsen-Anhalt, SH=Schleswig-Holstein, TH=Thüringen. Mehrere Komma-getrennte Angaben möglich (z.B. re=TH,BW).
+
+**Parameter:** *bt* (Optional)
+- 0
+- 1
+- 2
+- 3
+- 4
+- 5
+
+Beginntermin (0=regelmäßiger Start, 1=diesen Monat, 2=Folgemonat, 3=in zwei Monaten, 4=in drei Monaten, 5=in mehr als drei Monaten)
+
+**Parameter:** *uz* (Optional)
+- 1
+- 2
+
+Unterrichtszeit: 1=Vollzeit, 2=Teilzeit. Mehrere Komma-getrennte Angaben möglich (z.B. uz=1,2)
+
+
+**Parameter:** *dauer* (Optional)
+- 1,2
+- 1,2,3
+- 1,2,3,4
+- 1,2,3,4,5
+- 1,2,3,4,5,6
+- 7,8,9
+- 0
+
+Dauer: 0=Auf Anfrage, 1,2=bis eine Woche, 1,2,3=bis ein Monat, 1,2,3,4=bis drei Monate, 1,2,3,4,5=bis sechs Monate, 1,2,3,4,5,6=bis ein Jahr, 7,8,9=mehr als ein Jahr
+
+
+**Parameter:** *uf* (Optional)
+- 101
+- 102
+- 103
+- 104
+- 105
+- 201
+- 202
+- 203
+- 204
+- 206
+- 301
+- 302
+- 303
+- 304
+- 401
+- 402
+- 403
+
+Unterrichtsform: 101=Präsenzveranstaltung, 102=Seminar, 103=Workshop, 104=Praxistraining, 105=Sonstige Präsenzveranstaltung, 201=Virtuelles Klassenzimmer, 202=Online-Seminar, 203=Online-Coaching, 204=Selbstlernmodul, 206=Sonstige digitale Lernformen, 301=Blended Learning, 302=Combined Learning, 303=Hybrid Learning, 304=Sonstige kombinierte Lernformen,401=Fernunterricht, 402=Fernlehrgang, 403=Sonstiger Fernunterricht. Mehrere Komma-getrennte Angaben möglich (z.B. uf=101,202).
+
+
+**Parameter:** *anbieter* (Optional)
+Anbieter-ID (z.B. 22210). 
+
+
+**Parameter:** *it* (Optional)
+- RC
+- RD
+
+Integrationstyp: RC=Ausbildung Reha, RD=weiterbildung Reha. Mehrere Komma-getrennte Angaben möglich (z.B. re=RC,RD).
+
+
+**Parameter:** *bg* (Optional)
+- true
+- false
+
+Bildungsgutschein: true=nur Angebote mit Zulassung zur Förderung mit Bildungsgutschein anzeigen, false=nicht nur Angebote mit Zulassung zur Förderung mit Bildungsgutschein anzeigen.
+
+Beispiel:
+```bash
+jobs=$(curl -m 60 -H "Host: rest.arbeitsagentur.de" \
+-H "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0" \
+-H "Accept: application/json, text/plain, */*" \
+-H "Accept-Language: de,en-US;q=0.7,en;q=0.3" \
+-H "Accept-Encoding: gzip, deflate, br" \
+-H "Origin: https://web.arbeitsagentur.de" \
+-H "DNT: 1" \
+-H "Connection: keep-alive" \
+-H "Pragma: no-cache" \
+-H "Cache-Control: no-cache" \
+-H "OAuthAccessToken: $token" \
+'https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=0')
+```
---
+++ api_example.R
@@ -0,0 +1,241 @@
+
+#------------------------------------------------------------------
+# Get clientID & clientSecret from https://web.arbeitsagentur.de/weiterbildungssuche/suche?ort=Feucht_90537_11.224918_49.376701&uk=Bundesweit
+#------------------------------------------------------------------
+# GET /weiterbildungssuche/suche?ort=Feucht_90537_11.224918_49.376701&uk=Bundesweit HTTP/1.1
+# Host: web.arbeitsagentur.de
+# User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0
+# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+# Accept-Language: de,en-US;q=0.7,en;q=0.3
+# Accept-Encoding: gzip, deflate, br
+# DNT: 1
+# Connection: keep-alive
+# Upgrade-Insecure-Requests: 1
+# Pragma: no-cache
+# Cache-Control: no-cache
+#------------------------------------------------------------------
+
+getCredentials=function(
+    url="https://web.arbeitsagentur.de/weiterbildungssuche/suche?ort=Feucht_90537_11.224918_49.376701&uk=Bundesweit",
+    verbose=T
+    ){ 
+    if(verbose) print("\nTrying to get credentials...\n")
+    get_request=httr::GET(
+        url=url,
+        config=httr::config(connecttimeout=60) #config=httr::timeout(60)
+        )
+    x=as.character(httr::content(get_request)) 
+    clientId=print(gsub("(.* '|',)","",qqBaseX::matchAll(x,"clientId: (.*?),")))
+    clientSecret=print(gsub("(.* '|',)","",qqBaseX::matchAll(x,"clientSecret: (.*?),")))
+    if(verbose){
+        print(paste("URL: ", url))
+        print(paste("Credentials unchanged?", clientId=="38053956-6618-4953-b670-b4ae7a2360b1" & clientSecret=="c385073c-3b97-42a9-b916-08fd8a5d1795"))
+    }
+    return(list(
+        clientId=gsub("(.* '|',)","",qqBaseX::matchAll(x,"clientId: (.*?),")),
+        clientSecret=gsub("(.* '|',)","",qqBaseX::matchAll(x,"clientSecret: (.*?),")),
+        clientCookies=get_request$cookies$value,
+        data=get_request
+    ))
+}
+
+
+#------------------------------------------------------------------
+# Get token from https://rest.arbeitsagentur.de/oauth/gettoken_cc
+#------------------------------------------------------------------
+# POST /oauth/gettoken_cc HTTP/1.1
+# Host: rest.arbeitsagentur.de
+# User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0
+# Accept: application/json, text/plain, */*
+# Accept-Language: de,en-US;q=0.7,en;q=0.3
+# Accept-Encoding: gzip, deflate, br
+# Content-Type: application/x-www-form-urlencoded
+# Content-Length: 127
+# Origin: https://web.arbeitsagentur.de
+# DNT: 1
+# Connection: keep-alive
+# Pragma: no-cache
+# Cache-Control: no-cache
+#------------------------------------------------------------------
+
+getToken=function(
+    url="https://rest.arbeitsagentur.de/oauth/gettoken_cc",
+    clientId="38053956-6618-4953-b670-b4ae7a2360b1",
+    clientSecret="c385073c-3b97-42a9-b916-08fd8a5d1795",
+    clientCookies=NULL,
+    verbose=T
+    ){
+    if(verbose) print("\nTrying to get token...\n")
+    postHeaders=c(
+        "Host"="rest.arbeitsagentur.de",
+        "User-Agent"="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Accept"="application/json, text/plain, */*",
+        "Accept-Language"="de,en-US;q=0.7,en;q=0.3",
+        "Accept-Encoding"="gzip, deflate, br",
+        "Content-Type"="application/x-www-form-urlencoded",
+        "Content-Length"="127",
+        "Origin"="https://web.arbeitsagentur.de",
+        "DNT"="1",   
+        "Connection"="keep-alive",
+        "Pragma"="no-cache",
+        "Cache-Control"="no-cache"
+    )
+    if(!is.null(clientCookies)) postHeaders=c(postHeaders,"Cookie"=clientCookies)
+    postData=list( 
+        "grant_type"="client_credentials",
+        "client_id"=clientId,
+        "client_secret"=clientSecret
+    ) 
+    token_request=httr::POST(
+        url=url,
+        body=postData,encode="form",
+        httr::add_headers(.headers = c(postHeaders)),
+        config=httr::config(connecttimeout=60) #config=httr::timeout(60)
+    )
+    token_body <- httr::content(token_request, as='parsed')
+    if(verbose){
+        print(paste("URL:", url))
+        print(paste("Token:", token_body$access_token))
+        print(paste("x-Correlation-Id:", token_request$headers$"x-correlationid"))
+        print(paste("Cookies:", token_request$cookies$value))
+    }
+    return(list(
+        access_token=token_body$access_token,
+        correlationId=token_request$headers$"x-correlationid",
+        clientCookies=token_request$cookies$value,
+        data=token_request))
+}
+
+
+#------------------------------------------------------------------
+# Get Data from https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=...
+#------------------------------------------------------------------
+# GET /infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=1 HTTP/1.1
+# Host: rest.arbeitsagentur.de
+# User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0
+# Accept: application/json, text/plain, */*
+# Accept-Language: de,en-US;q=0.7,en;q=0.3
+# Accept-Encoding: gzip, deflate, br
+# OAuthAccessToken: eyJhbGciOiJIUzUxMiJ9...
+# Correlation-ID: a1EkTJO89Wtq
+# Origin: https://web.arbeitsagentur.de
+# DNT: 1
+# Connection: keep-alive
+# Cookie: rest=024dae17b4-4300-45vOKiLdHVOHS_arV35aZPckQ9HCcIUZc79U521-Jhej62BfRwhdxgpA-N_G3231g0oNI; _pk_id.1060.cfae=8d47d6e7e29d5156.1643703603.1.1643703603.1643703603.; _pk_ses.1060.cfae=1
+# Pragma: no-cache
+# Cache-Control: no-cache
+#------------------------------------------------------------------
+
+getData=function(url="https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=0",      
+        accessToken="eyJhbGciOiJIUzUxMiJ9.eyAic3ViIjogIlI0amhFWGdOWW1yL21Cd1lFTi9oR0N...",
+        correlationId=NULL, #"9c326b27-ac5d-96cf-106a-d455563f1096",
+        clientCookies=NULL,
+        verbose=T){
+    if(verbose) print("\nTrying to get data...\n")
+    getHeaders=c(
+        "Host"="rest.arbeitsagentur.de",
+        "User-Agent"="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+        "Accept"="application/json, text/plain, */*",
+        "Accept-Language"="de,en-US;q=0.7,en;q=0.3",
+        "Accept-Encoding"="gzip, deflate, br",        
+        "Origin"="https://web.arbeitsagentur.de",
+        "DNT"="1",
+        "Connection"="keep-alive",
+        "Pragma"="no-cache",
+        "Cache-Control"="no-cache"
+    )
+    if(!is.null(accessToken)) getHeaders=c(getHeaders,"OAuthAccessToken"=accessToken)
+    if(!is.null(correlationId)) getHeaders=c(getHeaders,"Correlation-ID"=correlationId)
+    if(!is.null(clientCookies)) getHeaders=c(getHeaders,"Cookie"=clientCookies)
+    data_request=httr::GET(
+        url=url, 
+        httr::add_headers(.headers=getHeaders),
+        config=httr::config(connecttimeout=60) #config=httr::timeout(60)
+    )
+    if(verbose){
+        print(paste("URL:", url))
+        print(nchar(rawToChar(httr::content(data_request))))
+        print(substr(rawToChar(httr::content(data_request)),1,100))
+
+    }
+    return(
+        data_request
+    )
+}
+
+
+#------------------------------------------------------------------
+# Demo: get one page
+#------------------------------------------------------------------
+
+dataList=list()
+if(F){ #get page 0
+    url=paste0("https://web.arbeitsagentur.de/weiterbildungssuche/suche?ort=Feucht_90537_11.224918_49.376701&uk=Bundesweit&seite=0")
+    credentials=getCredentials(url)
+    token=getToken(
+        url="https://rest.arbeitsagentur.de/oauth/gettoken_cc",
+        clientId=credentials[[1]],
+        clientSecret=credentials[[2]])
+    data=getData(
+        url=paste0("https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=0"),        
+        accessToken=token[[1]])
+        #correlationId=token[[2]])
+    hexCodedData=httr::content(data)
+    decodedData=rawToChar(hexCodedData)
+    dataList[["page 0"]]=decodedData
+
+    writeLines(decodedData,paste0(Sys.Date(),"_kursnetData_",0,".txt"))
+    save.image(paste0(Sys.Date(),"_kursnet_successfulRequest.RData"))
+}
+
+
+#------------------------------------------------------------------
+# Demo: Get several pages
+#------------------------------------------------------------------
+
+i=0;
+token=NULL
+if(F)
+while(T){ # pages 1-499?    
+    url=paste0("https://web.arbeitsagentur.de/weiterbildungssuche/suche?ort=Feucht_90537_11.224918_49.376701&uk=Bundesweit&seite=",i)
+    credentials=getCredentials(url)
+    if(i/50==round(i/50))token=NULL # get a new token, once in a while
+    if(is.null(token)){
+        token=getToken(
+            url="https://rest.arbeitsagentur.de/oauth/gettoken_cc",
+            clientId=credentials[[1]],
+            clientSecret=credentials[[2]])
+    }
+    data=getData(
+        url=paste0("https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=",i),
+                   #https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=2
+        accessToken=token[[1]],
+        correlationId=token[[2]],
+        clientCookies=token[[3]])
+    hexCodedData=httr::content(data)
+    decodedData=rawToChar(hexCodedData)
+    dataList[[paste0("page ",i)]]=decodedData
+
+    writeLines(decodedData,paste0(Sys.Date(),"_kursnetData_",i,".txt"))
+    save.image(paste0(Sys.Date(),"_kursnet_successfulRequest.RData"))
+    max=rjson::fromJSON(decodedData)[[3]][[3]]
+    print(paste(i,"<",max))
+    i=i+1
+    if(i>=max)break;
+}
+
+#--------------
+# Explore data
+#--------------
+
+if(F){
+    decodedData=dataList[[1]]
+    jsonData=rjson::fromJSON(decodedData)
+    names(jsonData)
+    angebote=sapply(jsonData[[1]][[1]],function(x)paste(x,collapse="\n"))
+    seitenzahl=(jsonData[[3]][[3]])
+    str(jsonData[[4]])
+    anbieter=sapply(jsonData[[4]],function(x)paste(x,collapse="\n"))
+}
+
+
