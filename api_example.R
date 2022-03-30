d6479c865f3e6c73e092128cbb1d815064bfd71c
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2022-03-30 15:01:58 +0200

    Update api_example.R
    Add a simpler example

--- api_example.R
+++ api_example.R
@@ -1,5 +1,21 @@
+#----------------
+# Simple Example
+#----------------
 install.packages(c("devtools","rjson","httr"))
 devtools::install_github("AndreasFischer1985/qqBaseX")
+clientId="38053956-6618-4953-b670-b4ae7a2360b1"
+clientSecret="c385073c-3b97-42a9-b916-08fd8a5d1795"
+postData=list( "grant_type"="client_credentials","client_id"=clientId,"client_secret"=clientSecret) 
+token_request=httr::POST(
+        url="https://rest.arbeitsagentur.de/oauth/gettoken_cc",
+        body=postData,encode="form",
+        config=httr::config(connecttimeout=60))
+token=httr::content(token_request, as='parsed')$access_token
+url="https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot?orte=Feucht_90537_11.224918_49.376701&uk=Bundesweit&bg=false&page=0"
+data_request=httr::GET(url=url, httr::add_headers(.headers=c("OAuthAccessToken"=token)),
+        config=httr::config(connecttimeout=60))
+data=rawToChar(httr::content(data_request))
+
 
 #------------------------------------------------------------------
 # Get clientID & clientSecret from https://web.arbeitsagentur.de/weiterbildungssuche/suche?ort=Feucht_90537_11.224918_49.376701&uk=Bundesweit
