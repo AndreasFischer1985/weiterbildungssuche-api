b8d2e6c5ef985bf787abc4ab03eda460e4d0a8f3
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2022-11-23 13:18:41 +0100

    removed dependency on rjson

--- api_example.R
+++ api_example.R
@@ -1,7 +1,7 @@
 #----------------
 # Simple Example
 #----------------
-install.packages(c("devtools","rjson","httr"))
+install.packages(c("devtools","httr","jsonlite"))
 devtools::install_github("AndreasFischer1985/qqBaseX")
 clientId="38053956-6618-4953-b670-b4ae7a2360b1"
 clientSecret="c385073c-3b97-42a9-b916-08fd8a5d1795"
@@ -14,6 +14,6 @@ token=httr::content(token_request, as='parsed')$access_token
 url="https://rest.arbeitsagentur.de/infosysbub/wbsuche/pc/v1/bildungsangebot"
 data_request=httr::GET(url=url, httr::add_headers(.headers=c("OAuthAccessToken"=token)),
         config=httr::config(connecttimeout=60))
-data=rjson::fromJSON(rawToChar(httr::content(data_request)))
+data=jsonlite::fromJSON(rawToChar(httr::content(data_request)))
 
-writeLines(jsonlite::toJSON(data1$aggregations,pretty=TRUE,auto_unbox=TRUE),paste0(Sys.Date(),"_wbsuche_aggregations.json"))
+writeLines(jsonlite::toJSON(data$aggregations,pretty=TRUE,auto_unbox=TRUE),paste0(Sys.Date(),"_wbsuche_aggregations.json"))
